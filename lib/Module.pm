13b7aa1b0aa136e8949625285001dafd7b5fd8e3
Roy Storey <kiwiroy@users.noreply.github.com>
2019-01-24 01:11:51 +1300

    Create Module.pm

---
+++ lib/Module.pm
@@ -0,0 +1,21 @@
+package Module;
+
+use strict;
+use warnings;
+use feature 'say';
+
+sub new { say "new"; }
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Module - A Perl Module
+
+=head1 DESCRIPTION
+
+Light on.
+
+=cut
