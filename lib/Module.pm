19114e77ccb909850ad9d7e992ef3e22ddebea5c
Roy Storey <kiwiroy@users.noreply.github.com>
2019-01-25 11:00:26 +1300

    squashed

--- lib/Module.pm
+++ lib/Module.pm
@@ -1,10 +1,4 @@
-package Module;
-
-use strict;
-use warnings;
-use feature 'say';
-
-sub new { say "new"; }
+package Applify;
 
 1;
 
@@ -12,10 +6,40 @@ sub new { say "new"; }
 
 =head1 NAME
 
-Module - A Perl Module
+Module - Test
 
-=head1 DESCRIPTION
+=head1 METHODS
 
-Light on.
+These are they.
+
+=head2 one
+
+The first one.
+
+=over 4
+
+=item parameter
+
+Application. See L</SYNOPSIS> for example code. This function can also be
+called as a method on C<$self>.
+
+=item two
+
+two
+
+=back
+
+=head1 APPLICATION CLASS
+
+This module will generate an application class, which C<$self> inside the
+L</app> block refer to. This class will have:
+
+=over 4
+
+=item * one
+
+one
+
+=back
 
 =cut
